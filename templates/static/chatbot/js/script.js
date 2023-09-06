082a96269199c3069eef1daf529e5764da230657
Gabriel Mango <gabrielhmango@gmail.com>
2023-09-06 12:09:49 -0300

    configuração da resposta da api

--- templates/static/chatbot/js/script.js
+++ templates/static/chatbot/js/script.js
@@ -11,6 +11,20 @@ document.addEventListener("DOMContentLoaded", function () {
             newMessage.innerHTML = `<strong>Você:</strong> ${messageText}`;
             messageBox.appendChild(newMessage);
 
+            fetch(`http://127.0.0.1:8050/chatbot/${encodeURIComponent(messageText)}`)
+                .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Erro na solicitação: ${response.status}`);
+                }
+                return response.json();
+                })
+                .then((data) => {
+                console.log(data.message); // Isso imprimirá a resposta do chatbot no console
+                })
+                .catch((error) => {
+                console.error(`Erro: ${error}`);
+                });
+
             // Limpar a caixa de entrada
             messageInput.value = "";
 
